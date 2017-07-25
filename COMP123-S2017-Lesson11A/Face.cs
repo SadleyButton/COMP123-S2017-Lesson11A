4d900fa9497f02cf03f6033aa931847809dd6baf
SadleyButton <brad.kh.sutton@gmail.com>
2017-07-25 15:18:20 -0400

    Created Enum for Suit and Face

---
+++ COMP123-S2017-Lesson11A/Face.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// Name: Bradley Sutton
+/// Date: July 25, 2017
+/// Description: Enum for card face and values
+/// Version 0.1 - Created the card face and starting enum value
+/// </summary>
+
+namespace COMP123_S2017_Lesson11A
+{
+    public enum Face
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
--- COMP123-S2017-Lesson11A/Suit.cs
+++ COMP123-S2017-Lesson11A/Suit.cs
@@ -2,21 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 /// <summary>
 /// Name: Bradley Sutton
 /// Date: July 25, 2017
-/// Description: Lesson 11 A
-/// Version 0.1 - Created the program class
+/// Description: Enum for all Suits
+/// Version 0.1 - Created the Enum and suit values
 /// </summary>
 
 namespace COMP123_S2017_Lesson11A
 {
-    class Program
+    public enum Suit
     {
-        static void Main(string[] args)
-        {
-        }
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
     }
 }
