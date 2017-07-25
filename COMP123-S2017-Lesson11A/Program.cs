8ccba3008708f5cd47b965e0d0a9fb15b227904f
SadleyButton <brad.kh.sutton@gmail.com>
2017-07-25 16:30:27 -0400

    Added syntax to main (use and display shuffle)

--- COMP123-S2017-Lesson11A/Program.cs
+++ COMP123-S2017-Lesson11A/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 /// Name: Bradley Sutton
 /// Date: July 25, 2017
 /// Description: Lesson 11 A
-/// Version 0.1 - Created the program class
+/// Version 0.2 - Added syntax to shuffle deck and display the shuffled results
 /// </summary>
 
 namespace COMP123_S2017_Lesson11A
@@ -18,6 +18,8 @@ namespace COMP123_S2017_Lesson11A
         static void Main(string[] args)
         {
             Deck deck = new Deck();
+            Console.WriteLine(deck.ToString());
+            deck.Shuffle();
             Console.WriteLine(deck.ToString());
         }
     }
