0e9298ef3790d4bcac2e211f0b40ff665e89128a
SadleyButton <brad.kh.sutton@gmail.com>
2017-07-25 15:33:11 -0400

    Created the Deck Class; inherit and is a list of type Card

---
+++ COMP123-S2017-Lesson11A/Deck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// Name: Bradley Sutton
+/// Date: July 25, 2017
+/// Description: This is the Deck Class
+/// Version 0.1 - This is the Desk Class which inherits from the Card Class
+///             - This class creates a new List type of class Card
+/// </summary>
+
+namespace COMP123_S2017_Lesson11A
+{
+    public class Deck:List<Card>
+    {
+        //PRIVATE INSTANCE VARIABLES
+
+        //PUBLIC VARIABLES
+
+        //CONSTRUCTORS --------------------------------------------------------------------------
+
+        //PRIVATE METHODS
+
+        //PUBLIC METHODS
+    }
+}
