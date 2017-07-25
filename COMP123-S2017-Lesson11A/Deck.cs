af013906aed9f957a12dd0dc940cea6c7f93c9b1
SadleyButton <brad.kh.sutton@gmail.com>
2017-07-25 15:53:14 -0400

    Created Deck constructor, private method (load deck) and ToString to output results of deck.

--- COMP123-S2017-Lesson11A/Deck.cs
+++ COMP123-S2017-Lesson11A/Deck.cs
@@ -7,8 +7,9 @@ using System.Text;
 /// Name: Bradley Sutton
 /// Date: July 25, 2017
 /// Description: This is the Deck Class
-/// Version 0.1 - This is the Desk Class which inherits from the Card Class
-///             - This class creates a new List type of class Card
+/// Version 0.2 - Created the main constructor for the Deck Class
+///             - Created a private method to load a deck of 52 cards
+///             - Created a ToString method to output the current deck result
 /// </summary>
 
 namespace COMP123_S2017_Lesson11A
@@ -20,9 +21,45 @@ namespace COMP123_S2017_Lesson11A
         //PUBLIC VARIABLES
 
         //CONSTRUCTORS --------------------------------------------------------------------------
+        /// <summary>
+        /// This is the main constructor for the Deck Class
+        /// Takes no arguments
+        /// </summary>
+        public Deck()
+        {
+            this._initialize();
+        }
 
         //PRIVATE METHODS
+        /// <summary>
+        /// This private method (_initalize) that loads a deck with 52 cards
+        /// </summary>
+        private void _initialize()
+        {
+            for (int suit = 0; suit < (int)Suit.Spades; suit++)
+            {
+                for (int face = 1; face < (int)Face.King; face++)
+                {
+                    this.Add(new Card((Face)face, (Suit)suit));
+                }
+            }            
+        }
 
         //PUBLIC METHODS
+        /// <summary>
+        /// This method overrides the public ToString method and outputs the current Deck contents.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string outputstring = "";
+
+            foreach (Card card in this)
+            {
+                outputstring += "The " + card.Face + " of " + card.Suit + "\n";
+            }
+
+            return outputstring;
+        }
     }
 }
